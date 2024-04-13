460cdd1a1d3662bbc16199c996ae3b97a0415aa0
akimasa-l <liumingzheng8@outlook.com>
2024-04-13 16:39:25 +0900

    Initial Commit

---
+++ mytimer/mytimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mytimerApp.swift
+//  mytimer
+//
+//  Created by 劉明正 on 2024/04/13.
+//
+
+import SwiftUI
+
+@main
+struct mytimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
