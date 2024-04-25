eafb3e627040c2a4f442e3125f2882d3d86ddff8
akimasa-l <liumingzheng8@outlook.com>
2024-04-25 12:49:38 +0900

    できたねえ

--- mytimer/ContentView.swift
+++ mytimer/ContentView.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 
+class TimerModel: ObservableObject{
+    var timer:Timer?
+    @Published var count=0
+    func timerStart(){
+        timer=Timer.scheduledTimer(withTimeInterval: 1, repeats: true){
+            _ in
+            self.count+=1
+        }
+    }
+}
 struct ContentView: View {
+    @EnvironmentObject var timer:TimerModel
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("count:\(timer.count)")
         }
         .padding()
+        .onAppear{
+            self.timer.timerStart()
+        }
     }
 }
 
